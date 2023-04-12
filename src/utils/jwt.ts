1e591cf76c4a4bf3a0347862a9a4dc31a54c18c2
Amune Israel <amuneisrael224@gmail.com>
2023-04-12 17:37:33 +0100

    made changes

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 
 
 const PrivateKey = config.get<string>("PrivateKey")
-//const PublicKey = config.get<string>("PublicKey")
   
 export function signJwt({_id,email}:{_id:mongoose.Types.ObjectId,email:string}){
    
@@ -15,23 +14,3 @@ export function signJwt({_id,email}:{_id:mongoose.Types.ObjectId,email:string}){
  return jwt.sign({_id: _id, email: email},PrivateKey,signOptions)
 }
 
-
-
-// export function verifyJwt(acessToken:string){
-// try {
-//    const decoded =  jwt.verify(acessToken,PublicKey)
-//    if(decoded){
-//     return {
-//         decoded,
-//         valid : true,
-//         expired:false
-//     }
-//    }
-// } catch (error:any) {
-//     return {
-//         decoded : null,
-//         valid : ( error.name === "TokenExpiredError"),
-//         expired: false
-//     }
-// }
-// }
